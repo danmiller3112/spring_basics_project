03f03cb268416f580b70b1ae5d94c5d7a0969d67
Denys Romanchenko <6104172@gmail.com>
2017-04-21 17:28:03 +0300

    + add loggers.xml

---
+++ src/main/java/core/loggers/CashFileEventLogger.java
@@ -0,0 +1,41 @@
+package core.loggers;
+
+import core.beans.Event;
+
+import java.util.ArrayList;
+import java.util.List;
+
+/**
+ * Created by RDL on 21/04/2017.
+ */
+public class CashFileEventLogger extends FileEventLogger {
+
+    private int cacheSize;
+    private List<Event> cache;
+
+    public CashFileEventLogger(String filename, int cacheSize) {
+        super(filename);
+        this.cacheSize = cacheSize;
+        this.cache = new ArrayList<>();
+    }
+
+    public void destroy() {
+        if (!cache.isEmpty()) {
+            writeEventsFromCache();
+        }
+    }
+
+    @Override
+    public void logEvent(Event event) {
+        cache.add(event);
+
+        if (cache.size() == cacheSize) {
+            writeEventsFromCache();
+            cache.clear();
+        }
+    }
+
+    private void writeEventsFromCache() {
+        cache.stream().forEach(super::logEvent);
+    }
+}
---
+++ src/main/java/core/loggers/FileEventLogger.java
@@ -0,0 +1,36 @@
+package core.loggers;
+
+import core.beans.Event;
+
+import java.io.File;
+import java.io.FileWriter;
+import java.io.IOException;
+
+/**
+ * Created by RDL on 21/04/2017.
+ */
+public class FileEventLogger implements EventLogger {
+    private String filename;
+    private File file;
+
+    public FileEventLogger(String filename) {
+        this.filename = filename;
+    }
+
+    private void init() throws IOException{
+        this.file = new File(filename);
+        if (!file.canWrite()){
+            throw new IOException("Can't write file " + filename);
+        }
+
+    }
+
+    public void logEvent(Event event) {
+        try (FileWriter fileWriter = new FileWriter(file, true)) {
+            fileWriter.write(event.toString());
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+
+    }
+}
